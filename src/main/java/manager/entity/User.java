5a2d06e5bf345afd12df6f6f81bbb1cccc74a269
jonathan Diaz <jonatham01@hotmail.com>
2025-05-07 18:31:04 -0500

    Role, RolePermission, and User Classes are created

---
+++ src/main/java/manager/entity/User.java
@@ -0,0 +1,63 @@
+package manager.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name="auth_users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+    private String name;
+    private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return  true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ src/main/java/manager/util/RolePermission.java
@@ -0,0 +1,14 @@
+package manager.util;
+
+public enum RolePermission {
+
+    READ_ALL,
+    READ_ONE,
+    CREATE_ONE,
+    UPDATE_ONE,
+    DISABLE_ONE,
+
+    READ_MY_PURCHASES,
+    READ_MY_PROFILE;
+
+}
