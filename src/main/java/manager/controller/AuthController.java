432da26bbc85179f8035f30bcff43116216d3652
jonathan Diaz <jonatham01@hotmail.com>
2025-05-08 09:43:12 -0500

    authentication controller is created

---
+++ src/main/java/manager/controller/AuthController.java
@@ -0,0 +1,47 @@
+package manager.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import manager.dto.AuthenticationRequest;
+import manager.dto.AuthenticationResponse;
+import manager.dto.UserRequestDTO;
+import manager.dto.UserResponseDTO;
+import manager.entity.User;
+import manager.service.AuthService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("authentication")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping
+    public ResponseEntity<UserResponseDTO> createNewUser(@RequestBody UserRequestDTO dto) {
+        try{ return ResponseEntity.ok(authService.registerUser(dto));
+        }catch (Exception e){ throw  new RuntimeException("System could not create new user"); }
+    }
+    @PostMapping("authenticate")
+    public ResponseEntity<AuthenticationResponse> login(@RequestBody AuthenticationRequest authenticationRequest) {
+        try{ return ResponseEntity.ok(authService.login(authenticationRequest));
+        }catch (Exception e){ throw  new RuntimeException("System could not authenticate user"); }
+    }
+
+    @GetMapping
+    public ResponseEntity<String> logOut(HttpServletRequest request) {
+        authService.logout(request);
+        return ResponseEntity.ok("logout successful");
+    }
+
+    @PreAuthorize("hasAuthority('READ_MY_PROFILE')")
+    @GetMapping("/profile")
+    public ResponseEntity<User> getProfile(){
+        User user= authService.findLoggedInUser();
+        return  ResponseEntity.ok(user);
+    }
+
+
+}
